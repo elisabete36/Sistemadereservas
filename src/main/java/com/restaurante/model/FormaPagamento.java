3eef70566aaf095765a924db3822b99ee23dd406
elisabete36 <104171527+elisabete36@users.noreply.github.com>
2025-04-13 22:31:01 -0300

    Add src/main/java/com/restaurante/model/FormaPagamento.java 1744594260586

--- src/main/java/com/restaurante/model/FormaPagamento.java
+++ src/main/java/com/restaurante/model/FormaPagamento.java
@@ -1,50 +1,51 @@
-package FormaPagamento.model;
+package com.restaurante.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.*;
 
 @Entity
+@Table(name = "formas_pagamento")
 public class FormaPagamento {
+    public enum Tipo {
+        CARTAO_CREDITO, 
+        CARTAO_DEBITO, 
+        DINHEIRO, 
+        PIX
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Tipo tipo;
+
+    @Column(length = 100)
     private String descricao;
-    private boolean ativo;
 
-    // Construtores
-    public FormaPagamento() {
-    }
+    @OneToMany(mappedBy = "formaPagamento", cascade = CascadeType.ALL)
+    private List<Reserva> reservas = new ArrayList<>();
 
-    public FormaPagamento(String descricao, boolean ativo) {
+    public FormaPagamento() {}
+
+    public FormaPagamento(Tipo tipo, String descricao) {
+        this.tipo = tipo;
         this.descricao = descricao;
-        this.ativo = ativo;
     }
 
     // Getters e Setters
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public Tipo getTipo() { return tipo; }
+    public void setTipo(Tipo tipo) { this.tipo = tipo; }
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+    public List<Reserva> getReservas() { return reservas; }
+    public void setReservas(List<Reserva> reservas) { this.reservas = reservas; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public boolean isAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
+    @Override
+    public String toString() {
+        return tipo.toString() + (descricao != null ? " (" + descricao + ")" : "");
     }
 }
